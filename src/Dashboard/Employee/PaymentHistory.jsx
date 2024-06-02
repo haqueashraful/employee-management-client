c13c6b6dcab8fcbdd7e172550b11daa885bb6132
haqueashraful <123ashrafulhaque@gmail.com>
2024-06-02 12:18:09 +0600

    payment history page added:

---
+++ src/Dashboard/Employee/PaymentHistory.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { Table } from "antd";
+import useAuth from "../Hooks/useAuth";
+import useAxiosPublic from "../Hooks/useAxiosPublic";
+import { useQuery } from "@tanstack/react-query";
+
+const PaymentHistory = () => {
+  const { user } = useAuth();
+  const axiosPublic = useAxiosPublic();
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const { data: payments = [], isLoading } = useQuery({
+    queryKey: ["payments", currentPage, user?.email],
+    queryFn: async () => {
+      const res = await axiosPublic.get("/payments", {
+        params: {
+          page: currentPage,
+        },
+      });
+      return res.data;
+    },
+  });
+
+  const columns = [
+    {
+      title: "Month",
+      dataIndex: "month",
+      key: "month",
+    },
+    {
+      title: "Amount",
+      dataIndex: "amount",
+      key: "amount",
+    },
+    {
+      title: "Transaction Id",
+      dataIndex: "transactionId",
+      key: "transactionId",
+    },
+  ];
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center mb-5">Payment History</h1>
+      <Table
+        dataSource={payments}
+        columns={columns}
+        rowKey={(record) => record.transactionId}
+        pagination={{ pageSize: 5, onChange: setCurrentPage }}
+      />
+    </div>
+  );
+};
+
+export default PaymentHistory;
